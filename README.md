4a52a870043ea436dd40c17a45210731052c7279
jbclaudiogs <claudio@justbetter.nl>
2021-02-26 12:59:15 +0100

    Init module

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rapidez Review
+
+## Installation
+
+```
+composer require rapidez/review
+```
+
+If you haven't published the Rapidez views yet, publish them with:
+```
+php artisan vendor:publish --provider="Rapidez\Core\RapidezServiceProvider" --tag=views
+```
---
+++ src/ReviewServiceProvider.php
@@ -0,0 +1,26 @@
+
+<?php
+
+namespace Rapidez\Review;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class ReviewServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'rapidez');
+
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/rapidez'),
+        ], 'views');
+    }
+}
