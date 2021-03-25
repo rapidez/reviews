89812daf6f98c940a191f6f17af775f17cd3aec6
jbclaudiogs <claudio@justbetter.nl>
2021-03-25 19:09:42 +0100

    Removed unnecessary template

--- resources/views/product/review-stars.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="flex items-start">
-    <div class="flex items-center">
-        <graphql v-cloak query='@include('review::queries.reviews', ['sku' => $sku])'>
-            <div class="flex items-center" slot-scope="{ data }" v-if="data">
-                <product-rating :rating_summary="data.products.items[0].rating_summary" :list="true" />
-            </div>
-        </graphql>
-    </div>
-</div>
