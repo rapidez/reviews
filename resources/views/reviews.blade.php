110fb4874930582adf71a57f602659607515fc19
Roy Duineveld <royduineveld@gmail.com>
2021-09-10 12:34:56 +0200

    Text color for better contrast

--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -4,7 +4,7 @@
         <div v-for="review in data.products.items[0].reviews.items" class="w-full bg-gray-200 rounded p-3 mt-3">
             <p class="flex items-baseline">
                 <strong>@{{ review.nickname }}</strong>
-                <span class="ml-2 text-primary text-xs">@{{ new Date(review.created_at).toLocaleDateString() }}</span>
+                <span class="ml-2 text-xs">@{{ new Date(review.created_at).toLocaleDateString() }}</span>
             </p>
             <stars class="mt-1" :score="review.average_rating"></stars>
             <strong class="block mt-3">@{{ review.summary }}</strong>
