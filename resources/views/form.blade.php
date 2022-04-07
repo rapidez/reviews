afddcbf5179c48b43a41f9cbf8e66ee803c58e03
Roy Duineveld <royduineveld@gmail.com>
2022-04-07 14:27:04 +0200

    Lazy loaded components

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,33 +1,35 @@
-<graphql v-cloak query='@include('rapidez-reviews::queries.ratingsMetadata')'>
-    <div v-if="data" slot-scope="{ data }">
-        <x-rapidez::recaptcha location="product_review"/>
-        <graphql-mutation query="mutation review ($sku: String!, $nickname: String!, $summary: String!, $text: String!, $ratings: [ProductReviewRatingInput!]!) { createProductReview ( input: { sku: $sku, nickname: $nickname, summary: $summary, text: $text, ratings: $ratings } ), { review { nickname summary text average_rating ratings_breakdown { name value } } } }" :variables="{ ratings: [], sku: '{{ $sku }}' }" :clear="true" :recaptcha="{{ Rapidez::config('recaptcha_frontend/type_for/product_review') == 'recaptcha_v3' ? 'true' : 'false' }}">
-            <form slot-scope="{ variables, mutate, mutated }" v-on:submit.prevent="mutate">
-                <div class="w-full max-w-xl bg-white rounded-lg pt-2">
-                    <strong class="text-1xl">@lang('Add Your Review')</strong>
-                    <div class="flex flex-wrap w-full">
-                        <div class="w-full md:max-w-xl">
-                            <div class="mb-2" v-for="(rating, index) in data.productReviewRatingsMetadata.items">
-                                <x-rapidez::label>@{{ rating.name }}</x-rapidez::label>
-                                <star-input v-model="variables.ratings[index]" :rating="rating"></star-input>
+<lazy-component>
+    <graphql v-cloak query='@include('rapidez-reviews::queries.ratingsMetadata')'>
+        <div v-if="data" slot-scope="{ data }">
+            <x-rapidez::recaptcha location="product_review"/>
+            <graphql-mutation query="mutation review ($sku: String!, $nickname: String!, $summary: String!, $text: String!, $ratings: [ProductReviewRatingInput!]!) { createProductReview ( input: { sku: $sku, nickname: $nickname, summary: $summary, text: $text, ratings: $ratings } ), { review { nickname summary text average_rating ratings_breakdown { name value } } } }" :variables="{ ratings: [], sku: '{{ $sku }}' }" :clear="true" :recaptcha="{{ Rapidez::config('recaptcha_frontend/type_for/product_review') == 'recaptcha_v3' ? 'true' : 'false' }}">
+                <form slot-scope="{ variables, mutate, mutated }" v-on:submit.prevent="mutate">
+                    <div class="w-full max-w-xl bg-white rounded-lg pt-2">
+                        <strong class="text-1xl">@lang('Add Your Review')</strong>
+                        <div class="flex flex-wrap w-full">
+                            <div class="w-full md:max-w-xl">
+                                <div class="mb-2" v-for="(rating, index) in data.productReviewRatingsMetadata.items">
+                                    <x-rapidez::label>@{{ rating.name }}</x-rapidez::label>
+                                    <star-input v-model="variables.ratings[index]" :rating="rating"></star-input>
+                                </div>
+                                <div class="space-y-2">
+                                    <x-rapidez::input v-model="variables.nickname" name="nickname" required/>
+                                    <x-rapidez::input v-model="variables.summary" name="summary" required/>
+                                    <x-rapidez::textarea v-model="variables.text" name="review" required/>
+                                </div>
                             </div>
-                            <div class="space-y-2">
-                                <x-rapidez::input v-model="variables.nickname" name="nickname" required/>
-                                <x-rapidez::input v-model="variables.summary" name="summary" required/>
-                                <x-rapidez::textarea v-model="variables.text" name="review" required/>
+                            <div class="w-full flex items-center mt-2">
+                                <x-rapidez::button type="submit">
+                                    @lang('Submit Review')
+                                </x-rapidez::button>
+                                <span v-if="mutated" class="ml-3 text-green-500">
+                                    @lang('You submitted your review for moderation.')
+                                </span>
                             </div>
-                        </div>
-                        <div class="w-full flex items-center mt-2">
-                            <x-rapidez::button type="submit">
-                                @lang('Submit Review')
-                            </x-rapidez::button>
-                            <span v-if="mutated" class="ml-3 text-green-500">
-                                @lang('You submitted your review for moderation.')
-                            </span>
                         </div>
                     </div>
-                </div>
-            </form>
-        </graphql-mutation>
-    </div>
-</graphql>
+                </form>
+            </graphql-mutation>
+        </div>
+    </graphql>
+</lazy-component>
