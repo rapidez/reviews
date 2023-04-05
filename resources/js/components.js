d0828bb6a0ed294af9428a4ae5aeed71d175e3d5
indykoning <15870933+indykoning@users.noreply.github.com>
2023-04-05 10:27:12 +0200

    Add package.js (#12)

---
+++ resources/js/components.js
@@ -0,0 +1,4 @@
+import 'Vendor/rapidez/core/resources/js/vue'
+
+Vue.component('star-input', () => import('./components/StarInput.vue'))
+Vue.component('stars', () => import('./components/Stars.vue'))
