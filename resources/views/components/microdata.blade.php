62512d4996f4e2189a8f39fbbf8668faa20a3267
Roy Duineveld <royduineveld@gmail.com>
2026-01-23 16:30:13 +0100

    Fixes

--- resources/views/components/microdata.blade.php
+++ resources/views/components/microdata.blade.php
@@ -1,7 +1,7 @@
-@if($product->reviews_score)
+@if($product->reviewSummary?->rating_summary)
     <div itemprop="aggregateRating" itemtype="https://schema.org/AggregateRating" itemscope>
-        <meta itemprop="reviewCount" content="{{ $product->reviews_count }}" />
-        <meta itemprop="ratingValue" content="{{ $product->reviews_score }}" />
+        <meta itemprop="reviewCount" content="{{ $product->reviewSummary->reviews_count }}" />
+        <meta itemprop="ratingValue" content="{{ $product->reviewSummary->rating_summary }}" />
         <meta itemprop="bestRating" content="100" />
     </div>
 @endif
